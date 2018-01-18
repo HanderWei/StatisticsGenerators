a86d306a5891173c9d6bd11c561dff2a75a9d9f9
Hander <hander_wei@163.com>
2018-01-19 00:54:10 +0800

    完成埋点脚本编写

---
+++ generator.py
@@ -0,0 +1,35 @@
+from parser import parse_file
+
+
+def generate_code_file(input_file, table_name, output_file):
+    """生成代码文件"""
+    with open(output_file, 'wb') as f:
+        rows = parse_file(input_file, table_name)
+        for row in rows:
+            generate_map(f, row)
+
+
+def generate_code_file(input_file, table_name, output_file, start, end):
+    """生成代码文件"""
+    with open(output_file, 'wb') as f:
+        rows = parse_file(input_file, table_name, start, end)
+        for row in rows:
+            generate_map(f, row)
+
+
+def generate_map(file, row):
+    """生成Map字段"""
+    file.write(str("//" + row['condition'].strip() + "\n").encode('utf-8'))
+    data = row['data']
+    file.write("Map<String, String> map = new HashMap<>();\n".encode('utf-8'))
+    for (k, v) in data.items():
+        map_str = str("map.put(\"%s\", \"%s\")\n" % (k, v))
+        file.write(map_str.encode('utf-8'))
+    trace_args = (row['id'], row['label'])
+    trace_str = ".getInstance().traceLogAndDa(\"%s\", \"%s\", map)\n\n" % trace_args
+    file.write(trace_str.encode('utf-8'))
+
+
+# generate_code_file('statistics.xlsx', 'Android', 'generate_code.txt')
+generate_code_file('statistics.xlsx', 'Android', '课程页.txt', 34, 51)
+generate_code_file('statistics.xlsx', 'Android', '学习页.txt', 59, 68)
--- parser.py
+++ parser.py
@@ -1,15 +1,55 @@
 from openpyxl import load_workbook
+import re
 
-wb = load_workbook(filename='statistics.xlsx')
-ws = wb['Android']
+key_value_p = re.compile('\w*[:]+\s*\w*')
 
-i = 0
 
-for row in ws.rows:
-    if row[7].value is not None:
-        print(row[7].value)
-    # if len(row) > 6:
-    #     for cell in row:
-    #         i = i + 1
-    #         args = (i, cell.value)
-    #         print("%d: %s" % args)
+def parse_file(file_name, table_name):
+    """解析文件, 返回数据字典列表"""
+    wb = load_workbook(filename=file_name)
+    ws = wb[table_name]
+    result_list = []
+    for row in ws.rows:
+        if should_parse_this_row(row):
+            result_list.append(parse_row(row))
+    return result_list
+
+
+def parse_file(file_name, table_name, start, end):
+    """解析文件, 返回数据字典列表"""
+    wb = load_workbook(filename=file_name)
+    ws = wb[table_name]
+    result_list = []
+    for row in ws.iter_rows(min_row=start, max_row=end):
+        if should_parse_this_row(row):
+            result_list.append(parse_row(row))
+    return result_list
+
+
+def should_parse_this_row(row):
+    """判断当前row是否符合解析的要求"""
+    if row[7].value is not None and str(row[7].value).strip()[0] == '{' and str(row[7].value).strip()[-1] == '}':
+        return True
+    else:
+        return False
+
+
+def parse_row(row):
+    """解析列"""
+    return {'id': str(row[0].value).strip(), 'version': str(row[1].value).strip(),
+            'condition': str(row[2].value).strip(), 'category': str(row[3].value).strip(),
+            'eventId': str(row[4].value).strip(), 'label': str(row[5].value).strip(),
+            'actionId': str(row[6].value).strip(), 'data': get_key_values(str(row[7].value).strip())}
+
+
+def get_key_values(kv_str):
+    """解析表格中最后一项"""
+    kv_str = kv_str.lstrip('{').rstrip('}')
+    kws_list = key_value_p.findall(kv_str)
+    kwargs = {}
+    for kw_str in kws_list:
+        words = kw_str.split(':')
+        key = words[0].strip()
+        value = words[-1].strip()
+        kwargs[key] = value
+    return kwargs
